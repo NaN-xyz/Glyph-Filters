98e08c4e752374c880d607df8ada0e3b2640fa2a
Simon Cozens <simon@simon-cozens.org>
2020-11-23 14:55:35 +0000

    Reformat with black

--- Scripts/04-Maze.py
+++ Scripts/04-Maze.py
@@ -1,6 +1,6 @@
-#MenuTitle: 04. Maze
+# MenuTitle: 04. Maze
 # -*- coding: utf-8 -*-
-__doc__="""
+__doc__ = """
 04. Maze
 """
 
@@ -11,115 +11,121 @@ from NaNFilter import NaNFilter
 
 
 class Maze(NaNFilter):
-	params = {
-		"S": { "offset": 5 },
-		"M": { "offset": 10 },
-		"L": { "offset": 10 },
-	}
+    params = {
+        "S": {"offset": 5},
+        "M": {"offset": 10},
+        "L": {"offset": 10},
+    }
 
-	def setup(self):
-		self.unit = 30
+    def setup(self):
+        self.unit = 30
 
-	def processLayer(self, thislayer, params):
-		offset = params["offset"]
-		if ContainsPaths(thislayer):
-			offsetpaths = self.saveOffsetPaths(thislayer, offset, offset, removeOverlap=False)
-			pathlist = doAngularizzle(offsetpaths, 4)
-			outlinedata = setGlyphCoords(pathlist)
+    def processLayer(self, thislayer, params):
+        offset = params["offset"]
+        if ContainsPaths(thislayer):
+            offsetpaths = self.saveOffsetPaths(
+                thislayer, offset, offset, removeOverlap=False
+            )
+            pathlist = doAngularizzle(offsetpaths, 4)
+            outlinedata = setGlyphCoords(pathlist)
 
-			bounds = AllPathBounds(thislayer)
-			self.setupChecker(bounds)
-			self.setAvailableSlots(thislayer, outlinedata)
+            bounds = AllPathBounds(thislayer)
+            self.setupChecker(bounds)
+            self.setAvailableSlots(thislayer, outlinedata)
 
-			ClearPaths(thislayer)
+            ClearPaths(thislayer)
 
-			walkpaths = self.walkerLoop(thislayer)
-			AddAllPathsToLayer(walkpaths, thislayer)
-			
-			self.expandMonoline(thislayer, 6)
-			thislayer.removeOverlap()
+            walkpaths = self.walkerLoop(thislayer)
+            AddAllPathsToLayer(walkpaths, thislayer)
 
-	def setupChecker(self, bounds):
-		self.available_slots = []
-		self.ox, self.oy, self.ow, self.oh = bounds
-		self.ysteps = self.oh // self.unit
-		self.xsteps = self.ow // self.unit
-		self.checker = []
+            self.expandMonoline(thislayer, 6)
+            thislayer.removeOverlap()
 
-		for stepy in range(0, self.ysteps+3):
-			xlist = []
-			for stepx in range(0, self.xsteps+3):
-				xlist.append(True)
-			self.checker.append(xlist)
+    def setupChecker(self, bounds):
+        self.available_slots = []
+        self.ox, self.oy, self.ow, self.oh = bounds
+        self.ysteps = self.oh // self.unit
+        self.xsteps = self.ow // self.unit
+        self.checker = []
 
+        for stepy in range(0, self.ysteps + 3):
+            xlist = []
+            for stepx in range(0, self.xsteps + 3):
+                xlist.append(True)
+            self.checker.append(xlist)
 
-	def setAvailableSlots(self, thislayer, outlinedata):
-		for stepy in range(0, self.ysteps+3, 1):
+    def setAvailableSlots(self, thislayer, outlinedata):
+        for stepy in range(0, self.ysteps + 3, 1):
 
-			y = self.oy + (stepy*self.unit)
+            y = self.oy + (stepy * self.unit)
 
-			for stepx in range(0, self.xsteps+3, 1):
+            for stepx in range(0, self.xsteps + 3, 1):
 
-				x = self.ox + (stepx*self.unit)
-				shapepath = []
-				nx, ny = x+self.unit/2, y+self.unit/2
-				shape = drawTriangle(nx, ny, 6, 6)
-				shapepath.append(shape)
+                x = self.ox + (stepx * self.unit)
+                shapepath = []
+                nx, ny = x + self.unit / 2, y + self.unit / 2
+                shape = drawTriangle(nx, ny, 6, 6)
+                shapepath.append(shape)
 
-				nshape = doAngularizzle(shapepath, 10)
-				nshape = setGlyphCoords(nshape)
-				finalshape = nshape[0][1]
-				
-				if ShapeWithinOutlines(finalshape, outlinedata):
-					self.checker[stepy][stepx] = True
-					self.available_slots.append([stepx, stepy])
-				else:
-					self.checker[stepy][stepx] = False
+                nshape = doAngularizzle(shapepath, 10)
+                nshape = setGlyphCoords(nshape)
+                finalshape = nshape[0][1]
 
-	def updateChecker(self, xpos, ypos):
-		self.checker[ypos][xpos] = False
+                if ShapeWithinOutlines(finalshape, outlinedata):
+                    self.checker[stepy][stepx] = True
+                    self.available_slots.append([stepx, stepy])
+                else:
+                    self.checker[stepy][stepx] = False
 
-		item = [xpos, ypos]
-		if item in self.available_slots:
-			self.available_slots.remove(item)
+    def updateChecker(self, xpos, ypos):
+        self.checker[ypos][xpos] = False
 
-	def walkerLoop(self, thislayer):
-		walkerpaths = []
+        item = [xpos, ypos]
+        if item in self.available_slots:
+            self.available_slots.remove(item)
 
-		while len(self.available_slots)>0:
-			start = random.choice(self.available_slots)
-			self.updateChecker(start[0], start[1])
-			walks = self.walker(thislayer, start)
-			walkerpaths.extend(walks)
+    def walkerLoop(self, thislayer):
+        walkerpaths = []
 
-		return walkerpaths
+        while len(self.available_slots) > 0:
+            start = random.choice(self.available_slots)
+            self.updateChecker(start[0], start[1])
+            walks = self.walker(thislayer, start)
+            walkerpaths.extend(walks)
 
-	def walker(self, thislayer, start):
-		movements = { "N": (0,1), "S": (0,-1), "E": (1,0), "W": (-1,0) }
-		walkpath = GSPath()
-		sx, sy = start
+        return walkerpaths
 
-		startnode = GSNode([self.ox+(sx*self.unit), self.oy+(sy*self.unit)], type = GSLINE)
-		walkpath.nodes.append( startnode )
+    def walker(self, thislayer, start):
+        movements = {"N": (0, 1), "S": (0, -1), "E": (1, 0), "W": (-1, 0)}
+        walkpath = GSPath()
+        sx, sy = start
 
-		breakcounter=0
-		walkerpaths = []
+        startnode = GSNode(
+            [self.ox + (sx * self.unit), self.oy + (sy * self.unit)], type=GSLINE
+        )
+        walkpath.nodes.append(startnode)
 
-		while breakcounter < 1000:
-			dx, dy = random.choice(movements.values())
-			lookx, looky = sx + dx, sy + dy
-			if [lookx, looky] in self.available_slots:
-				self.updateChecker(lookx, looky)
-				drawx, drawy = self.ox+(lookx*self.unit), self.oy+(looky*self.unit) 
-				walkpath.nodes.append( GSNode([drawx, drawy], type = GSLINE) )
-				sx, sy = lookx, looky
+        breakcounter = 0
+        walkerpaths = []
 
-			breakcounter+=1
+        while breakcounter < 1000:
+            dx, dy = random.choice(movements.values())
+            lookx, looky = sx + dx, sy + dy
+            if [lookx, looky] in self.available_slots:
+                self.updateChecker(lookx, looky)
+                drawx, drawy = self.ox + (lookx * self.unit), self.oy + (
+                    looky * self.unit
+                )
+                walkpath.nodes.append(GSNode([drawx, drawy], type=GSLINE))
+                sx, sy = lookx, looky
 
-		if (len(walkpath.nodes))==1: walkpath.nodes.append(startnode)
+            breakcounter += 1
 
-		walkerpaths.append(walkpath)
-		return walkerpaths
+        if (len(walkpath.nodes)) == 1:
+            walkpath.nodes.append(startnode)
+
+        walkerpaths.append(walkpath)
+        return walkerpaths
+
 
 Maze()
-
