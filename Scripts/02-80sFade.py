7590295cfb413b054783271df53644bb4cce9c18
Simon Cozens <simon@simon-cozens.org>
2020-11-23 14:03:17 +0000

    Put it inside the class, for neatness

--- Scripts/02-80sFade.py
+++ Scripts/02-80sFade.py
@@ -12,31 +12,6 @@ from NaNFilter import NaNFilter
 
 # THE MAIN CIRCLE FUNCTION ACTION
 
-def do80sFade(thislayer, outlinedata, tilecoords, shape_components):
-
-	b = AllPathBounds(thislayer)
-
-	if b is None:
-		return []
-
-	fadecomps = []
-	size=random.randrange(4,15)
-	r = random.choice(shape_components)
-	ox, oy, w, h = b[0], b[1], b[2], b[3]
-
-	for y in range(oy, oy+h, 20):
-
-		for x in range(ox, ox+w, 20):
-			if withinGlyphBlack(x, y, outlinedata) and point_inside_polygon(x, y, tilecoords) and size>2:
-				fadecomp = GSComponent(r)
-				scale = size / 100.0
-				fadecomp.transform = ((scale, 0.0, 0.0, scale, x, y))
-				#thislayer.components.append(fadecomp)
-				fadecomps.append(fadecomp)
-			size+=0.01
-
-	return fadecomps
-
 
 class EightiesFade(NaNFilter):
 	def processLayer(self, thislayer, params):
@@ -53,12 +28,36 @@ class EightiesFade(NaNFilter):
 			for n in range(0, len(allrectangles)):
 				x,y,w,h = allrectangles[n]
 				tilecoords = [[x,y], [x,y+h], [x+w,y+h], [x+w,y]]
-				fadecomps.extend(do80sFade(thislayer, outlinedata, tilecoords, shape_components) )
+				fadecomps.extend(self.do80sFade(thislayer, outlinedata, tilecoords, shape_components) )
 
 			ClearPaths(thislayer)
 			AddAllComponentsToLayer(fadecomps, thislayer)
 
 		except Exception as e:
 			print("Layer (", thislayer.name, ") failed to execute.", e)
+
+	def do80sFade(self, thislayer, outlinedata, tilecoords, shape_components):
+		b = AllPathBounds(thislayer)
+
+		if b is None:
+			return []
+
+		fadecomps = []
+		size=random.randrange(4,15)
+		r = random.choice(shape_components)
+		ox, oy, w, h = b[0], b[1], b[2], b[3]
+
+		for y in range(oy, oy+h, 20):
+
+			for x in range(ox, ox+w, 20):
+				if withinGlyphBlack(x, y, outlinedata) and point_inside_polygon(x, y, tilecoords) and size>2:
+					fadecomp = GSComponent(r)
+					scale = size / 100.0
+					fadecomp.transform = ((scale, 0.0, 0.0, scale, x, y))
+					#thislayer.components.append(fadecomp)
+					fadecomps.append(fadecomp)
+				size+=0.01
+
+		return fadecomps
 
 EightiesFade()
