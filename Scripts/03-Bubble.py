fbc5b6f2ff0cc7862aae5dc67c7b78ffaf91bafc
Simon Cozens <simon@simon-cozens.org>
2020-11-23 14:18:29 +0000

    Now the auxiliary function becomes superfluous

--- Scripts/03-Bubble.py
+++ Scripts/03-Bubble.py
@@ -10,62 +10,6 @@ from NaNGFAngularizzle import *
 from NaNFilter import NaNFilter
 
 
-def doBubble(thislayer, outlinedata, minpush, maxpush):
-
-	for path in outlinedata:
-
-		direction = path[0]
-		structure = path[1]
-		nodelen = len(structure)
-		bubble = GSPath()
-		n = 0
-
-		while n < nodelen:
-
-			x1 = structure[n][0]
-			y1 = structure[n][1]
-			minstep = 10
-			maxstep = 40
-
-			if direction=="False": maxstep=int(maxstep*0.5)
-			step = random.randrange(minstep, maxstep)
-
-			if n+step>=nodelen-1:
-				n = nodelen
-				break
-
-			# --- set node pos for main or end
-
-			if n<nodelen-1:
-				x2 = structure[n+step][0]
-				y2 = structure[n+step][1]
-				n+=step
-			else:
-				x2 = structure[0][0]
-				y2 = structure[0][1]
-
-			#if distance([x1, y1], [x2, y2]) < 1:
-			#	continue
-
-			a = atan2(y1-y2, x1-x2)
-			a += radians(90)
-			pushdist = random.randrange(minpush, maxpush) 
-			linex = pushdist * cos(a)
-			liney = pushdist * sin(a)
-
-			if direction=="False":
-				linex*=0.5
-				liney*=0.5
-
-			# DRAW THE POINTS
-			bubble.nodes.append(GSNode([x1+linex, y1+liney], type = GSOFFCURVE))
-			bubble.nodes.append(GSNode([x2+linex, y2+liney], type = GSOFFCURVE))
-			bubble.nodes.append(GSNode([x2, y2], type = GSCURVE))
-
-		bubble.closed = True
-		thislayer.paths.append(bubble)
-
-
 class Bubble(NaNFilter):
 	params = {
 		"S": { "offset": -5 },
@@ -79,6 +23,60 @@ class Bubble(NaNFilter):
 		pathlist = doAngularizzle(offsetpaths, 4)
 		outlinedata = setGlyphCoords(pathlist)
 		ClearPaths(thislayer)
-		doBubble(thislayer, outlinedata, 60, 80)
+		minpush = 60
+		maxpush = 80
+		for path in outlinedata:
+
+			direction = path[0]
+			structure = path[1]
+			nodelen = len(structure)
+			bubble = GSPath()
+			n = 0
+
+			while n < nodelen:
+
+				x1 = structure[n][0]
+				y1 = structure[n][1]
+				minstep = 10
+				maxstep = 40
+
+				if direction=="False": maxstep=int(maxstep*0.5)
+				step = random.randrange(minstep, maxstep)
+
+				if n+step>=nodelen-1:
+					n = nodelen
+					break
+
+				# --- set node pos for main or end
+
+				if n<nodelen-1:
+					x2 = structure[n+step][0]
+					y2 = structure[n+step][1]
+					n+=step
+				else:
+					x2 = structure[0][0]
+					y2 = structure[0][1]
+
+				#if distance([x1, y1], [x2, y2]) < 1:
+				#	continue
+
+				a = atan2(y1-y2, x1-x2)
+				a += radians(90)
+				pushdist = random.randrange(minpush, maxpush) 
+				linex = pushdist * cos(a)
+				liney = pushdist * sin(a)
+
+				if direction=="False":
+					linex*=0.5
+					liney*=0.5
+
+				# DRAW THE POINTS
+				bubble.nodes.append(GSNode([x1+linex, y1+liney], type = GSOFFCURVE))
+				bubble.nodes.append(GSNode([x2+linex, y2+liney], type = GSOFFCURVE))
+				bubble.nodes.append(GSNode([x2, y2], type = GSCURVE))
+
+			bubble.closed = True
+			thislayer.paths.append(bubble)
+
 
 Bubble()
