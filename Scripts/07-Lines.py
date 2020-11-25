a124c2b511fc2273770aacee8fdb9c79f268e56e
Simon Cozens <simon@simon-cozens.org>
2020-11-25 16:45:02 +0000

    Reformat with black

--- Scripts/07-Lines.py
+++ Scripts/07-Lines.py
@@ -1,6 +1,6 @@
-#MenuTitle: 07. Lines
+# MenuTitle: 07. Lines
 # -*- coding: utf-8 -*-
-__doc__="""
+__doc__ = """
 07. Lines
 """
 
@@ -9,73 +9,92 @@ from NaNGFGraphikshared import *
 from NaNGFAngularizzle import *
 from NaNFilter import NaNFilter
 
+
 class Lines(NaNFilter):
-	params = {
-		"S": { "offset": -5, "iterations":  50  },
-		"M": { "offset": -15, "iterations": 400 },
-		"L": { "offset": -20, "iterations": 420 }
-	}
+    params = {
+        "S": {"offset": -5, "iterations": 50},
+        "M": {"offset": -15, "iterations": 400},
+        "L": {"offset": -20, "iterations": 420},
+    }
 
-	def setup(self):
-		self.line_vertical_comp = CreateLineComponent(self.font, "vertical", 2, "LineVerticalComponent")
-		self.line_horizontal_comp = CreateLineComponent(self.font, "horizontal", 2, "LineHorizontalComponent")
+    def setup(self):
+        self.line_vertical_comp = CreateLineComponent(
+            self.font, "vertical", 2, "LineVerticalComponent"
+        )
+        self.line_horizontal_comp = CreateLineComponent(
+            self.font, "horizontal", 2, "LineHorizontalComponent"
+        )
 
-	def processLayer(self, thislayer, params):
+    def processLayer(self, thislayer, params):
 
-		outlinedata = setGlyphCoords(doAngularizzle(thislayer.paths, 40))
+        outlinedata = setGlyphCoords(doAngularizzle(thislayer.paths, 40))
 
-		try:
-			allrectangles = MakeRectangles([AllPathBounds(thislayer)], 5)
-			direction = "horizontal"
-			linecomps = []
+        try:
+            allrectangles = MakeRectangles([AllPathBounds(thislayer)], 5)
+            direction = "horizontal"
+            linecomps = []
 
-			for x,y,w,h in allrectangles:
-				tile = [x,y,w,h]
-				tilecoords = [[x,y], [x,y+h], [x+w,y+h], [x+w,y]]
+            for x, y, w, h in allrectangles:
+                tile = [x, y, w, h]
+                tilecoords = [[x, y], [x, y + h], [x + w, y + h], [x + w, y]]
 
-				if direction == "horizontal": direction="vertical"
-				else: direction="horizontal"
+                if direction == "horizontal":
+                    direction = "vertical"
+                else:
+                    direction = "horizontal"
 
-				linecomps.extend(self.DrawlinesTile(thislayer, outlinedata, tile, direction))
+                linecomps.extend(
+                    self.DrawlinesTile(thislayer, outlinedata, tile, direction)
+                )
 
-			ClearPaths(thislayer)
-			AddAllComponentsToLayer(linecomps, thislayer)
+            ClearPaths(thislayer)
+            AddAllComponentsToLayer(linecomps, thislayer)
 
-		except Exception as e:
-			print "Glyph (", thislayer.name, ") failed to execute:", e
+        except Exception as e:
+            print "Glyph (", thislayer.name, ") failed to execute:", e
 
-	def DrawlinesTile(self, thislayer, outlinedata, tile, direction):
+    def DrawlinesTile(self, thislayer, outlinedata, tile, direction):
 
-		x, y, w, h = [int(el) for el in tile]
-		tilecoords = [[x,y], [x,y+h], [x+w,y+h], [x+w,y]]
-		lines = []
-		linecomponents = []
-		gap = 20
-		checkgap = 2
+        x, y, w, h = [int(el) for el in tile]
+        tilecoords = [[x, y], [x, y + h], [x + w, y + h], [x + w, y]]
+        lines = []
+        linecomponents = []
+        gap = 20
+        checkgap = 2
 
-		self.newline = []
-		def add_line(x2, y2):
-			if withinGlyphBlack(x2, y2, outlinedata) and point_inside_polygon_faster(x2, y2, tilecoords):
-					self.newline.append([x2, y2])
-			else:
-				if len(self.newline)>1:
-					lines.append(self.newline)
-					self.newline = []
+        self.newline = []
 
-		if direction=="horizontal":
-			for y2 in range(y, y+h+gap, gap):
-				for x2 in range(x, x+w, checkgap):
-					add_line(x2,y2)
+        def add_line(x2, y2):
+            if withinGlyphBlack(x2, y2, outlinedata) and point_inside_polygon_faster(
+                x2, y2, tilecoords
+            ):
+                self.newline.append([x2, y2])
+            else:
+                if len(self.newline) > 1:
+                    lines.append(self.newline)
+                    self.newline = []
 
-		if direction=="vertical":
-			for x2 in range(x, x+w+gap, gap):
-				for y2 in range(y, y+h, checkgap):
-					add_line(x2,y2)
+        if direction == "horizontal":
+            for y2 in range(y, y + h + gap, gap):
+                for x2 in range(x, x + w, checkgap):
+                    add_line(x2, y2)
 
-		for l in lines:
-			comp = returnLineComponent(l[0], l[-1], direction, [self.line_vertical_comp,self.line_horizontal_comp], 100)
-			linecomponents.append(comp)
+        if direction == "vertical":
+            for x2 in range(x, x + w + gap, gap):
+                for y2 in range(y, y + h, checkgap):
+                    add_line(x2, y2)
 
-		return linecomponents
+        for l in lines:
+            comp = returnLineComponent(
+                l[0],
+                l[-1],
+                direction,
+                [self.line_vertical_comp, self.line_horizontal_comp],
+                100,
+            )
+            linecomponents.append(comp)
+
+        return linecomponents
+
 
 Lines()
