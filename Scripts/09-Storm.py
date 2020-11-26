b720dd70a4c55466222e001e48e21686ee7ef39e
Simon Cozens <simon@simon-cozens.org>
2020-11-26 16:22:19 +0000

    I feel like this could be a super useful abstraction

--- Scripts/09-Storm.py
+++ Scripts/09-Storm.py
@@ -10,26 +10,21 @@ from NaNGFAngularizzle import *
 from NaNGFNoise import *
 from NaNFilter import NaNFilter
 
-def drawStorm(thislayer, outlinedata, step, minsize, maxsize, stormcomponent):
-
-	b = AllPathBounds(thislayer)
+def operateOnBlackAtInterval(layer, outlinedata, func, step_x, step_y=None):
+	if step_y is None:
+		step_y = step_x
+	b = AllPathBounds(layer)
 	ox, oy, w, h = int(b[0])+1, b[1]+1, b[2], b[3]
-
-	freq = 0.005
-	for y in range(oy, oy+h, step):
-		for x in range(ox, ox+w, step):
+	results = []
+	for y in range(oy, oy+h, step_y):
+		for x in range(ox, ox+w, step_x):
 			if not withinGlyphBlack(x, y, outlinedata):
 				continue
-			noiz = snoise2(x*freq, y*freq, 3)
-			size = noiseMap( noiz, minsize, maxsize )
-			if size <= 4:
-				continue
-			stormcomp = GSComponent(stormcomponent)
-			scale = (float(1)/maxsize)*size
-			stormcomp.transform = ((scale, 0.0, 0.0, scale, x, y))
-			thislayer.components.append(stormcomp)
+			result = func(x,y,layer)
+			if result is not None:
+				results.extend(result)
 
-
+	return results
 
 class Storm(NaNFilter):
 	gridsize, minsize, maxsize = 25, 20, 60
@@ -37,10 +32,21 @@ class Storm(NaNFilter):
 	def setup(self):
 		self.stormcomponent = CreateShapeComponent(self.font, self.maxsize, self.maxsize, "circle", "StormShape")
 
+	def drawStorm(self, x,y,layer):
+		freq = 0.005
+		noiz = snoise2(x*freq, y*freq, 3)
+		size = noiseMap( noiz, self.minsize, self.maxsize )
+		if size <= 4:
+			return
+		stormcomp = GSComponent(self.stormcomponent)
+		scale = (float(1)/self.maxsize)*size
+		stormcomp.transform = ((scale, 0.0, 0.0, scale, x, y))
+		layer.components.append(stormcomp)
+
 	def processLayer(self, thislayer, params):
 		pathlist = doAngularizzle(thislayer.paths, 4)
 		outlinedata = setGlyphCoords(pathlist)
-		drawStorm(thislayer, outlinedata, self.gridsize, self.minsize, self.maxsize, self.stormcomponent)
+		operateOnBlackAtInterval(thislayer, outlinedata, self.drawStorm, self.gridsize)
 		ClearPaths(thislayer)
 
 Storm()
