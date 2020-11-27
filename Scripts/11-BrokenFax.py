12ef0be0a20c5770f2b137794b205ba669a754df
Simon Cozens <simon@simon-cozens.org>
2020-11-27 09:33:03 +0000

    Most of this is unnecessary????
    I was trying to understand what this did, so commented out the lines after AngularSteps/AddAllPathsToLayer to look at the intermediate output. And it looked exactly the same as the final output. So I am not convinced that the Shapefit step is necessary.

--- Scripts/11-BrokenFax.py
+++ Scripts/11-BrokenFax.py
@@ -41,61 +41,6 @@ def AngularSteps(thislayer, outlinedata, stepsize):
 
 	return [ drawSimplePath(path) for path in allpaths]
 
-def Shapefit(thislayer, outlinedata):
-
-	b = AllPathBounds(thislayer)
-
-	if b is None:
-		return
-
-	ox, oy, w, h = b[0], b[1], b[2], b[3]
-	sizes = returnSizes(random.randrange(3,5))
-	newshapes = []
-	allshapes = []
-
-	for shapesize in sizes:
-
-		for x in range(ox, ox+w, shapesize):
-
-			for y in range(oy, oy+h, shapesize):
-
-				shapepath = []
-				shape = drawRectangle(x, y, shapesize, shapesize)
-				shapepath.append(shape)
-				nshape = doAngularizzle(shapepath, 10)
-				nshape = setGlyphCoords(nshape)
-				finalshape = nshape[0][1]
-				rect = returnSquareShape(x, y, shapesize, shapesize)
-
-				if ShapeWithinOutlines(finalshape, outlinedata):
-					safe = True
-					for n in range(0, len(allshapes)):
-						if point_inside_polygon(x, y, allshapes[n]):
-							safe = False
-							break
-
-					if safe:
-						newshapes.append(shape)
-						allshapes.append(rect)	
-
-		return newshapes
-
-
-def returnSizes(it):
-
-	minsizes = [16, 24, 32, 48] #included 8
-	minit = random.choice(minsizes)
-	sizes = []
-	sizes.append(random.choice(minsizes))
-	for n in range(0, it): sizes.append(random.randrange(1,6)*minit)
-	return sizes
-
-
-def returnSquareShape(nx, ny, w, h):
-	coord = [ [nx,ny], [nx,ny+h], [nx+w,ny+h], [nx+w,ny] ]
-	return coord
-
-
 class BrokenFax(NaNFilter):
 	params = {
 		"S": {"offset": -20, "stepsize": 50 },
@@ -111,12 +56,6 @@ class BrokenFax(NaNFilter):
 
 		angularpaths = AngularSteps(thislayer, outlinedata, params["stepsize"])
 		AddAllPathsToLayer(angularpaths, thislayer)
-
-		offsetpaths = saveOffsetPaths(thislayer, params["offset"], params["offset"], removeOverlap=True)
-		pathlist = doAngularizzle(offsetpaths, 4)
-		outlinedata = setGlyphCoords(pathlist)
-		shapepaths = Shapefit(thislayer, outlinedata)
-		AddAllPathsToLayer(shapepaths, thislayer)
 		thislayer.removeOverlap()
 
 BrokenFax()
