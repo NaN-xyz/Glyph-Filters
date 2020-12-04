45eddb86c3c6c70da4b7416c7ae4ab45ac50408f
Simon Cozens <simon@simon-cozens.org>
2020-12-04 09:52:46 +0000

    Now this method can be folded in

--- Scripts/19-SprayPaint.py
+++ Scripts/19-SprayPaint.py
@@ -12,69 +12,6 @@ from noise import *
 from NaNFilter import NaNFilter
 
 
-# THE MAIN SPRAY PAINT ACTION
-
-def doSpray(thislayer, paths):
-
-	for path in paths:
-		# only round shape if over certain size (for small forms)
-		if isPathSizeBelowThreshold(path,120,120):
-			structure = path
-		else:
-			structure = convertToFitpath(RoundPath(path,"nodes"), True)
-		
-		pathlist = doAngularizzle([structure], 7)
-		outlinedata = setGlyphCoords(pathlist)
-
-		if not outlinedata:
-			continue
-
-		outlinedata = setGlyphCoords(pathlist)
-		structure = outlinedata[0][1]
-
-		nodelen = len(structure)
-
-		n = 0
-		newpath = []
-
-		noisescale = 0.01
-		segwaylen = 40
-		minshift, maxshift = 30, 90
-
-		seed = random.randrange(0,100000)
-		start_pushdist = 0
-		last_pushdist = 0
-
-		while n < nodelen:
-			x_prev, y_prev = structure[n-1]
-			x_curr, y_curr = structure[n]
-			x_next, y_next = structure[(n+1) % nodelen]
-
-			a1 = atan2(y_prev-y_next, x_prev-x_next)
-			a2 = a1+radians(90)
-
-			if n < nodelen-segwaylen:
-				pushdist = pnoise1( (n+seed)*noisescale, 3) 
-				pushdist = noiseMap( pushdist, minshift, maxshift )
-				last_pushdist = pushdist
-				if n==0:
-					start_pushdist = pushdist
-
-			else:
-				pushdist = last_pushdist + ( ( start_pushdist-last_pushdist ) / segwaylen ) * (segwaylen-(nodelen-n)) 
-
-			linex1, liney1 = MakeVector(pushdist, a2)
-			linex2, liney2 = MakeVector(pushdist, a1)
-
-			newpath.extend( [
-							[x_curr-linex2, y_curr-liney2], 
-							[x_curr+linex2, y_curr+liney2],
-							[x_curr+linex1, y_curr+liney1]]
-							)
-			n+=1
-
-		thislayer.paths.append(drawSimplePath(newpath))
-
 class Spray(NaNFilter):
 	params = {"S": { "offset": -5}, "M": { "offset": -10}, "L": { "offset": -20} }
 
@@ -84,7 +21,64 @@ class Spray(NaNFilter):
 
 		ClearPaths(thislayer)
 
-		doSpray(thislayer, pathlist)
+		for path in pathlist:
+			# only round shape if over certain size (for small forms)
+			if isPathSizeBelowThreshold(path,120,120):
+				structure = path
+			else:
+				structure = convertToFitpath(RoundPath(path,"nodes"), True)
+			
+			pathlist = doAngularizzle([structure], 7)
+			outlinedata = setGlyphCoords(pathlist)
+
+			if not outlinedata:
+				continue
+
+			outlinedata = setGlyphCoords(pathlist)
+			structure = outlinedata[0][1]
+
+			nodelen = len(structure)
+
+			n = 0
+			newpath = []
+
+			noisescale = 0.01
+			segwaylen = 40
+			minshift, maxshift = 30, 90
+
+			seed = random.randrange(0,100000)
+			start_pushdist = 0
+			last_pushdist = 0
+
+			while n < nodelen:
+				x_prev, y_prev = structure[n-1]
+				x_curr, y_curr = structure[n]
+				x_next, y_next = structure[(n+1) % nodelen]
+
+				a1 = atan2(y_prev-y_next, x_prev-x_next)
+				a2 = a1+radians(90)
+
+				if n < nodelen-segwaylen:
+					pushdist = pnoise1( (n+seed)*noisescale, 3) 
+					pushdist = noiseMap( pushdist, minshift, maxshift )
+					last_pushdist = pushdist
+					if n==0:
+						start_pushdist = pushdist
+
+				else:
+					pushdist = last_pushdist + ( ( start_pushdist-last_pushdist ) / segwaylen ) * (segwaylen-(nodelen-n)) 
+
+				linex1, liney1 = MakeVector(pushdist, a2)
+				linex2, liney2 = MakeVector(pushdist, a1)
+
+				newpath.extend( [
+								[x_curr-linex2, y_curr-liney2], 
+								[x_curr+linex2, y_curr+liney2],
+								[x_curr+linex1, y_curr+liney1]]
+								)
+				n+=1
+
+			thislayer.paths.append(drawSimplePath(newpath))
 
 Spray()
 
