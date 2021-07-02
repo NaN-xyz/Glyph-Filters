572982a3558b961cb05877c33a00cb60981e5539
Simon Cozens <simon@simon-cozens.org>
2021-07-02 10:22:46 +0100

    This can never have worked

--- NaNGlyphFilters/Gemstones.py
+++ NaNGlyphFilters/Gemstones.py
@@ -10,6 +10,17 @@ from NaNGFAngularizzle import *
 from NaNFilter import NaNFilter
 from NaNCommonFilters import moonrocks, spikes
 
+def drawSpike(gspath, x1, y1, midx, midy, x2, y2, pushdist):
+	gspath.nodes.append(GSNode([midx, midy], type = GSLINE))
+	gspath.nodes.append(GSNode([x2, y2], type = GSLINE))
+
+class Gemstones(NaNFilter):
+	params = {
+		"S": { "offset": -5, "minpush": 10, "maxpush": 20, "iterations": 50 },
+		"M": { "offset": -10, "minpush": 20, "maxpush": 40, "iterations": 400 },
+		"L": { "offset": -20, "minpush": 20, "maxpush": 40, "iterations": 420 },
+	}
+
 	def processLayer(self, thislayer, params):
 		offsetpaths = self.saveOffsetPaths(thislayer, params["offset"], params["offset"], removeOverlap=False)
 		pathlist = ConvertPathsToSkeleton(offsetpaths, 4)
@@ -26,16 +37,5 @@ from NaNCommonFilters import moonrocks, spikes
 		retractHandles(thislayer)
 
 		self.CleanOutlines(thislayer, remSmallPaths=True, remSmallSegments=True, remStrayPoints=True, remOpenPaths=True, keepshape=False)
-
-def drawSpike(gspath, x1, y1, midx, midy, x2, y2, pushdist):
-	gspath.nodes.append(GSNode([midx, midy], type = GSLINE))
-	gspath.nodes.append(GSNode([x2, y2], type = GSLINE))
-
-class Gemstones(NaNFilter):
-	params = {
-		"S": { "offset": -5, "minpush": 10, "maxpush": 20, "iterations": 50 },
-		"M": { "offset": -10, "minpush": 20, "maxpush": 40, "iterations": 400 },
-		"L": { "offset": -20, "minpush": 20, "maxpush": 40, "iterations": 420 },
-	}
 
 Gemstones()
