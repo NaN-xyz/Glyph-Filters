a118c472463e4c4e5129d322651530c0b17ab85a
Simon Cozens <simon@simon-cozens.org>
2023-07-04 21:12:24 +0100

    Remove * imports, run through black

--- NaNGlyphFilters/NaNGFConfig.py
+++ NaNGlyphFilters/NaNGFConfig.py
@@ -1,116 +1,151 @@
-from GlyphsApp import *
-import random
 from NaNGlyphsEnvironment import glyphsEnvironment as G
-from NaNGFGraphikshared import *
-from math import *
 import time
-import cProfile
 
-__all__ = ["beginFilterNaN", "beginGlyphNaN", "endGlyphNaN", "endFilterNaN", "glyphSize", "NANGFSET"]
+__all__ = [
+    "beginFilterNaN",
+    "beginGlyphNaN",
+    "endGlyphNaN",
+    "endFilterNaN",
+    "glyphSize",
+    "NANGFSET",
+]
 # Misc settings
 
 NANGFSET = {
-  "highlight_glyph": True,		# colour label the processed glyph
-  "highlight_col": 1,			# colour label # for orange
-  "show_time": G.is_interactive,			# show the time to process each glyph as well as total
-  "show_console": G.is_interactive,			# show macro and console window
-  "debug": True,				# LNP debug tool
-  "smallest_path": 7,			# Smallest permissable path width and height on clean-up
-  "smallest_seg": 5				# Smallest permissable segment length on clean-up
+    "highlight_glyph": True,  # colour label the processed glyph
+    "highlight_col": 1,  # colour label # for orange
+    "show_time": G.is_interactive,  # show the time to process each glyph as well as total
+    "show_console": G.is_interactive,  # show macro and console window
+    "debug": True,  # LNP debug tool
+    "smallest_path": 7,  # Smallest permissable path width and height on clean-up
+    "smallest_seg": 5,  # Smallest permissable segment length on clean-up
 }
 
 global start_time
 
 # SET UP COMMON STUFF
 
+
 def beginFilterNaN(font):
+    global start_time
+    selectedGlyphs = G.selected_glyphs(font)
+    selectedGlyphs = filterGSGlyphList(selectedGlyphs)
+    # print selectedGlyphs
 
-	global start_time
-	selectedGlyphs = G.selected_glyphs(font)
-	selectedGlyphs = filterGSGlyphList(selectedGlyphs)
-	#print selectedGlyphs
+    G.disable_updates(font)
+    if NANGFSET["show_time"]:
+        start_time = time.time()
+    if NANGFSET["show_console"]:
+        Glyphs.showMacroWindow()
+        Glyphs.clearLog()
+    return selectedGlyphs
 
-	G.disable_updates(font)
-	if NANGFSET["show_time"]==True: start_time = time.time()
-	if NANGFSET["show_console"]==True: 
-		Glyphs.showMacroWindow()
-		Glyphs.clearLog()
-	return selectedGlyphs
 
 def endFilterNaN(font):
-	G.enable_updates(font)
-	if NANGFSET["show_time"]==True: show_total_time(start_time)
+    G.enable_updates(font)
+    if NANGFSET["show_time"]:
+        show_total_time(start_time)
+
 
 def beginGlyphNaN(glyph):
+    global glyph_start_time, glyphsize
+    glyphsize = glyphSize(glyph)
 
-	global glyph_start_time, glyphsize
-	glyphsize=glyphSize(glyph)
+    if NANGFSET["show_time"]:
+        glyph_start_time = time.time()
+    if NANGFSET["debug"]:
+        print("starting glyph: " + glyph.name)
+        print("glyph size: " + glyphsize)
 
-	if NANGFSET["show_time"]==True: glyph_start_time = time.time()
-	if NANGFSET["debug"]==True: 
-		print("starting glyph: " + glyph.name)
-		print("glyph size: " + glyphsize)
 
 def endGlyphNaN(glyph):
+    if NANGFSET["show_time"]:
+        show_glyph_time(glyph_start_time, glyph.name)
+    if NANGFSET["highlight_glyph"]:
+        glyph.color = NANGFSET["highlight_col"]
+    print("-\n")
 
-	if NANGFSET["show_time"]==True: show_glyph_time(glyph_start_time, glyph.name)
-	if NANGFSET["highlight_glyph"]==True: glyph.color = NANGFSET["highlight_col"]
-	print("-\n")
+    # print out approximate time for entire glyphset
 
-	# print out approximate time for entire glyphset
 
 # SHOW INDIVIDUAL GLYPH PROCESSING TIME
 
+
 def show_glyph_time(start_time, glyph):
-	print("Processing time for glyph( " + glyph + " ): " + str(round(time.time()-start_time,4)) + " seconds")
+    print(
+        "Processing time for glyph( "
+        + glyph
+        + " ): "
+        + str(round(time.time() - start_time, 4))
+        + " seconds"
+    )
+
 
 # SHOW TOTAL PROCESSING TIME
 
+
 def show_total_time(start_time):
-	print("-\nTotal processing time: " + str(round(time.time()-start_time,4)) + " seconds, " + str(round((time.time()-start_time )/60,4)) + " minutes")
-	print("---\n")
+    print(
+        "-\nTotal processing time: "
+        + str(round(time.time() - start_time, 4))
+        + " seconds, "
+        + str(round((time.time() - start_time) / 60, 4))
+        + " minutes"
+    )
+    print("---\n")
 
 
 # RETURN A SIZE (L, M, S) IN ORDER REFINE LEVEL OF REDRAW
 def glyphSize(glyph):
+    cat = glyph.category
+    subcat = glyph.subCategory
+    nc = ""
 
-	cat =  glyph.category
-	subcat = glyph.subCategory
-	nc = ""
+    # decide glyph size based on category
+    if cat == "Letter":
+        if subcat == "Uppercase":
+            nc = "L"
+        else:
+            nc = "M"
+    elif cat == "Number":
+        if subcat == "Decimal Digit":
+            nc = "L"
+        elif subcat == "Small":
+            nc = "S"
+        else:
+            nc = "L"
+    elif cat == "Mark":
+        nc = "S"
+    elif cat == "Symbol":
+        if subcat == "Currency":
+            nc = "L"
+        elif subcat == "Math":
+            nc = "M"
+        else:
+            nc = "L"
+    elif cat == "Punctuation":
+        nc = "M"
+    else:
+        nc = "M"
 
-	# decide glyph size based on category
-	if cat=="Letter":
-		if subcat=="Uppercase": nc = "L"
-		else: nc = "M"
-	elif cat=="Number":
-		if subcat=="Decimal Digit": nc = "L"
-		elif subcat=="Small": nc = "S"
-		else: nc = "L"
-	elif cat=="Mark": nc = "S"
-	elif cat=="Symbol": 
-		if subcat=="Currency": nc = "L"
-		elif subcat=="Math": nc = "M"
-		else: nc = "L"
-	elif cat=="Punctuation": nc = "M"
-	else: nc = "M"
-
-	return nc
+    return nc
 
 
 def filterGSGlyphList(selectedG):
-	"""Return a list of unique GSGlyph objects, skipping those which cannot
-	be processed."""
-	newlist = []
-	seen = {}
-	for g in selectedG:
-		if g.name is None or g.category == "Separator" or g.name in seen:
-			continue
-		font = g.parent
-		if not ContainsPaths(font.glyphs[g.name].layers[0]):
-			continue
-		newlist.append(g)
-		seen[g.name] = True
-	return newlist
+    """Return a list of unique GSGlyph objects, skipping those which cannot
+    be processed."""
+    newlist = []
+    seen = {}
+    for g in selectedG:
+        if g.name is None or g.category == "Separator" or g.name in seen:
+            continue
+        font = g.parent
+        if not ContainsPaths(font.glyphs[g.name].layers[0]):
+            continue
+        newlist.append(g)
+        seen[g.name] = True
+    return newlist
+
 
 def ContainsPaths(thislayer):
-	return len(thislayer.paths)>0
+    return len(thislayer.paths) > 0
